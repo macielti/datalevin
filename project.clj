011aa0b8482d99ae2e16e2050aca6040773de5c3
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2026-01-26 18:35:41 -0300

    first commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject datalevin-component "0.1.0"
+  :description "FIXME: write description"
+  :url "https://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.12.2"]]
+  :repl-options {:init-ns datalevin-component.core})
---
+++ test/datalevin_component/core_test.clj
@@ -0,0 +1,7 @@
+(ns datalevin-component.core-test
+  (:require [clojure.test :refer :all]
+            [datalevin-component.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
